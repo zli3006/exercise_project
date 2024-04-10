9f52d17466ddfe5f39f241d03973a99ad61c8c41
zli3006 <71104212+zli3006@users.noreply.github.com>
2024-04-09 21:12:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var host = "cpsc484-01.stdusr.yale.internal:8888";
+
+$(document).ready(function () {
+    frames.start();
+});
+
+var frames = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + host + "/frames";
+        frames.socket = new WebSocket(url);
+        frames.socket.onmessage = function (event) {
+            frames.show(JSON.parse(event.data));
+        }
+    },
+
+    show: function (frame) {
+        console.log(frame);
+    }
+};
