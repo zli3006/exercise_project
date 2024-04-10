b724a32d64acb17227b74fdd07c879ee91c7cb9b
zli3006 <71104212+zli3006@users.noreply.github.com>
2024-04-10 15:09:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,36 @@ var frames = {
         frames.socket = new WebSocket(url);
         frames.socket.onmessage = function (event) {
             frames.show(JSON.parse(event.data));
-            // test change
         }
     },
 
     show: function (frame) {
         console.log(frame);
     }
+};
+
+$(document).ready(function() {
+  twod.start();
+});
+
+var twod = {
+    socket: null,
+
+    // create a connection to the camera feed
+    start: function () {
+        var url = "ws://" + host + "/twod";
+        twod.socket = new WebSocket(url);
+
+        // whenever a new frame is received...
+        twod.socket.onmessage = function (event) {
+
+            // parse and show the raw data
+            twod.show(JSON.parse(event.data));
+        }
+    },
+
+    // show the image by adjusting the source attribute of the HTML img object previously created
+    show: function (twod) {
+        $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
+    },
 };
