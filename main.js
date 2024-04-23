053d4da5c19f6ba00073cac54d15b7dc25a5da3c
Joseph Elsayyid <elsayyidj@gmail.com>
2024-04-23 13:57:50 -0400

    made changes to js and html

--- main.js
+++ main.js
@@ -1,71 +1,110 @@
 // main.js
+
 $(document).ready(function () {
     var host = "cpsc484-01.stdusr.yale.internal:8888";
-    var timeLeft = 3;
+    var timeLeft = 30;
     var exerciseStarted = false;
-    var start = 0;
     var option = 0;
-    var text = ""
-
+    var text = "";
+  
     $("#startButton").click(function () {
-        if (!exerciseStarted) {
-            startExercise();
-            exerciseStarted = true;
-            $(this).hide();
-            $('.exercise').show();
-        }
+      if (!exerciseStarted) {
+        startExercise();
+        exerciseStarted = true;
+        $(this).hide();
+        $('.exercise').show();
+      }
     });
-
+  
     function startExercise() {
-        option = option % 4
-        switch (option) {
-            case 0:
-                text = "Stretch 1: Raise your arms above your head and hold."
-                break;
-            case 1:
-                text = "Stretch 2"
-                break;
-            case 2:
-                text = "Stretch 3"
-                break;
-            case 3:
+      switch (option) {
+        case 0:
+          text = "Stretch 1: Raise your arms above your head and hold.";
+          break;
+        case 1:
+          text = "Stretch 2: Stretch your arms to the side and hold.";
+          break;
+        default:
+          endExercise();
+          return;
+      }
+  
+      $('#exerciseTitle').text(text);
+      $('#timer').text(timeLeft + " seconds remaining");
+  
+      var timerId = setInterval(function () {
+        if (timeLeft == -1) {
+          clearTimeout(timerId);
+          option++;
+          timeLeft = 30;
+          startExercise();
+        } else {
+          $('#timer').text(timeLeft + ' seconds remaining');
+          timeLeft--;
         }
-        $('#exerciseTitle').text("Stretch 1: Raise your arms and hold.");
-        $('#timer').text("3 seconds remaining");
-        var timerId = setInterval(function() {
-            if (timeLeft == -1) {
-                clearTimeout(timerId);
-                endExercise(); 
-            } else {
-                $('#timer').text(timeLeft + ' seconds remaining');
-                timeLeft--;
-            }
-        }, 1000);
-
-        initWebSocket();
+      }, 1000);
+  
+      initWebSocket();
     }
-
+  
     function endExercise() {
-        $('#exerciseTitle').text("Well done!");
-        $('#timer').text("Exercise complete.");
-        $('.exercise').hide();
-        $("#startButton").show();
-        exerciseStarted = false;
-        timeLeft = 3
+      $('#exerciseTitle').text("Thank you for completing the stretching routine!");
+      $('#timer').text("");
+      $('.exercise').hide();
+      $("#startButton").show();
+      exerciseStarted = false;
+      option = 0;
+      timeLeft = 30;
     }
-
+  
     function initWebSocket() {
-        var framesSocket = new WebSocket("ws://" + host + "/frames");
-        var twodSocket = new WebSocket("ws://" + host + "/twod");
-
-        framesSocket.onmessage = function (event) {
-            var data = JSON.parse(event.data);
-            // Logic to evaluate exercise performance goes here
-        };
-
-        twodSocket.onmessage = function (event) {
-            var data = JSON.parse(event.data);
-            $('img.twod').attr("src", 'data:image/jpeg;base64,' + data.src);
-        };
+      var framesSocket = new WebSocket("ws://" + host + "/frames");
+      var twodSocket = new WebSocket("ws://" + host + "/twod");
+  
+      framesSocket.onmessage = function (event) {
+        var data = JSON.parse(event.data);
+        var joints = data.bodies[0].joints;
+  
+        if (option === 0 && !isHandsAboveHead(joints)) {
+          $('#reminder').text("Please keep your hands above your head.");
+        } else if (option === 1 && !isHandsToSide(joints)) {
+          $('#reminder').text("Please keep your arms stretched to the side.");
+        } else {
+          $('#reminder').text("");
+        }
+      };
+  
+      twodSocket.onmessage = function (event) {
+        var data = JSON.parse(event.data);
+        $('img.twod').attr("src", 'data:image/jpeg;base64,' + data.src);
+      };
     }
-});
+  
+    function isHandsAboveHead(joints) {
+      var headJoint = joints.find(joint => joint.jointType === "Head");
+      var leftHandJoint = joints.find(joint => joint.jointType === "HandLeft");
+      var rightHandJoint = joints.find(joint => joint.jointType === "HandRight");
+  
+      if (headJoint && leftHandJoint && rightHandJoint) {
+        return leftHandJoint.position.y > headJoint.position.y && rightHandJoint.position.y > headJoint.position.y;
+      }
+  
+      return false;
+    }
+  
+    function isHandsToSide(joints) {
+      var leftShoulderJoint = joints.find(joint => joint.jointType === "ShoulderLeft");
+      var rightShoulderJoint = joints.find(joint => joint.jointType === "ShoulderRight");
+      var leftHandJoint = joints.find(joint => joint.jointType === "HandLeft");
+      var rightHandJoint = joints.find(joint => joint.jointType === "HandRight");
+  
+      if (leftShoulderJoint && rightShoulderJoint && leftHandJoint && rightHandJoint) {
+        var shoulderDistance = Math.abs(leftShoulderJoint.position.x - rightShoulderJoint.position.x);
+        var handDistance = Math.abs(leftHandJoint.position.x - rightHandJoint.position.x);
+  
+        return handDistance > shoulderDistance * 1.5;
+      }
+  
+      return false;
+    }
+  });
