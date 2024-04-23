234fe4b7289394d29f7ccdec33c14eb8d099b31c
zli3006 <71104212+zli3006@users.noreply.github.com>
2024-04-22 23:05:02 -0400

    Update main.js
    added exercise events

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // main.js
 $(document).ready(function () {
     var host = "cpsc484-01.stdusr.yale.internal:8888";
-    var timeLeft = 30;
+    var timeLeft = 3;
     var exerciseStarted = false;
+    var start = 0;
+    var option = 0;
+    var text = ""
 
     $("#startButton").click(function () {
         if (!exerciseStarted) {
@@ -14,10 +17,25 @@ $(document).ready(function () {
     });
 
     function startExercise() {
+        option = option % 4
+        switch (option) {
+            case 0:
+                text = "Stretch 1: Raise your arms and hold."
+                break;
+            case 1:
+                text = "Stretch 2"
+                break;
+            case 2:
+                text = "Stretch 3"
+                break;
+            case 3:
+        }
+        $('#exerciseTitle').text("Stretch 1: Raise your arms and hold.");
+        $('#timer').text("3 seconds remaining");
         var timerId = setInterval(function() {
             if (timeLeft == -1) {
                 clearTimeout(timerId);
-                endExercise();
+                endExercise(); 
             } else {
                 $('#timer').text(timeLeft + ' seconds remaining');
                 timeLeft--;
@@ -30,6 +48,10 @@ $(document).ready(function () {
     function endExercise() {
         $('#exerciseTitle').text("Well done!");
         $('#timer').text("Exercise complete.");
+        $('.exercise').hide();
+        $("#startButton").show();
+        exerciseStarted = false;
+        timeLeft = 3
     }
 
     function initWebSocket() {
